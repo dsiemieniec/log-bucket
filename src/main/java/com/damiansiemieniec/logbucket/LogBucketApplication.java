aa11cf1d2a4dcfe86b2f509e38906243f58fb5ec
Damian Siemieniec <damian.siemieniec@gmail.com>
2022-10-06 19:31:27 +0200

    Batch consumer

--- src/main/java/com/damiansiemieniec/logbucket/LogBucketApplication.java
+++ src/main/java/com/damiansiemieniec/logbucket/LogBucketApplication.java
@@ -3,9 +3,11 @@ package com.damiansiemieniec.logbucket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableIntegration
+@EnableKafka
 public class LogBucketApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/damiansiemieniec/logbucket/listener/MainTopicListener.java
+++ src/main/java/com/damiansiemieniec/logbucket/listener/MainTopicListener.java
@@ -1,13 +1,24 @@
 package com.damiansiemieniec.logbucket.listener;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MainTopicListener {
+    private static final int MAX_BATCH_SIZE = 100;
 
-    @KafkaListener(id = "mainTopicListener", topics = "mainTopic")
-    public void listen(String data) {
-        System.out.println("Listener received: " + data);
+    @KafkaListener(id = "mainTopicListener", topics = "mainTopic",  batch = "true", properties = {
+            ConsumerConfig.MAX_POLL_RECORDS_CONFIG + "=" + MAX_BATCH_SIZE
+    })
+    public void listen(List<String> messages) throws InterruptedException {
+        for (var d : messages) {
+            System.out.println("Listener received: " + d);
+        }
+
+        System.out.println("");
+        System.out.println("");
     }
 }
